0ff6a0bedd0dc61eb10cc3798874a621610f0fb0
Boaz <boaz23mw@gmail.com>
2022-07-28 16:02:00 +0300

    Translate control point: Only 1 segment

--- tutorial/Project/Bezier1d.h
+++ tutorial/Project/Bezier1d.h
@@ -6,7 +6,7 @@
 #include <Eigen/Core>
 
 template<typename TScalar, int PDim, int PFree> class Bezier1d;
-using Bezier1d_D_3_2 = Bezier1d<double, 3, 2>;
+// using Bezier1d_D_3_2 = Bezier1d<double, 3, 2>;
 
 template<typename TScalar, int PDim, int PFree> class Bezier1d
 {
@@ -18,20 +18,22 @@ public:
 
     using ph_matrix = Eigen::Matrix<TScalar, nControlPoints, PhDim>;
     using p_vector = Eigen::Matrix<TScalar, PDim, 1>;
+    using ph_vector = Eigen::Matrix<TScalar, PhDim, 1>;
 
 // TOOD: Initialize M and the default segment somehow
-//public:
-//    double dt;
-//    int degree;
-//    std::vector<ph_matrix> segments;
-//
-//    const static ph_matrix M;
+protected:
+    double dt;
+    std::vector<ph_matrix> segments;
 
 public:
-    //Bezier1d(int polyDegree, double dt = 1.0 / 16.0);
+    static ph_matrix M;
+    static ph_matrix DefaultSegment;
+
+public:
+    Bezier1d(double dt = 1.0 / 16.0) : dt{ dt }, segments{ DefaultSegment } {}
 
     ph_matrix GetControlPoints(int segment);
-    void TranslateControlPoint(int segment, int index, p_vector translation);
+    void TranslateControlPoint(int segment, int index, const p_vector &translation);
     void CreateSegment();
 
     p_vector GetPoint(int segment, float t);
@@ -39,33 +41,28 @@ public:
     void GetEdges(Eigen::Matrix<TScalar, Eigen::Dynamic, PDim> &P, Eigen::Matrix<int, Eigen::Dynamic, 2> &E);
 };
 
-template<> class Bezier1d<double, 3, 2>
+class Bezier1d_D_3_2 : public Bezier1d<double, 3, 2>
 {
 public:
-    const constexpr static int PDim = 3;
-    const constexpr static int PhDim = PDim + 1;
-    const constexpr static int PFree = 2;
-    const constexpr static int nControlPoints = 2 + 2;
-
-    using ph_matrix = Eigen::Matrix<double, nControlPoints, PhDim>;
-    using p_vector = Eigen::Matrix<double, PDim, 1>;
-
-private:
-    double dt;
-    int nPoints;
-    std::vector<ph_matrix> segments;
-
-    static ph_matrix M;
-    static ph_matrix DefaultSegment;
-
-public:
-    Bezier1d(int polyDegree, double dt = 1.0 / 16.0) : dt{ dt }, nPoints{ polyDegree + 1 }, segments{ DefaultSegment }
+    Bezier1d_D_3_2(double dt = 1.0 / 16.0) : Bezier1d<double, 3, 2>(dt)
     {
     }
 
     ph_matrix GetControlPoints(int segment)
     {
         return segments[segment];
+    }
+
+    void TranslateControlPoint(int segment, int index, const p_vector &translation)
+    {
+        if (segments.size() > 1)
+        {
+            throw std::exception("Operation not supported yet");
+        }
+
+        ph_vector t;
+        t << translation, 0;
+        segments[segment].row(index) += t;
     }
 };
 
